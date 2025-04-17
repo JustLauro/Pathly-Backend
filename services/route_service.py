3f24b466f18b79f16cb8b7bb3e6b6a870b255947
JustLauro <akkordeon165@gmail.com>
2025-04-17 11:27:39 +0200

    minor changes

--- services/route_service.py
+++ services/route_service.py
@@ -1,7 +1,9 @@
 from models.user_input import UserInput
 from apis.openAI import prompt_azure_ai
+from models.chatgpt_response import Route
 
 
 def generate_route(user_input: UserInput):
 
-    prompt_azure_ai(user_input.start_point, user_input.end_point, user_input.user_prompt)
+    generated_route: Route = prompt_azure_ai(user_input.start_point, user_input.end_point, user_input.user_prompt)
+    
